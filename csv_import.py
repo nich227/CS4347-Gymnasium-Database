b9303769cb4b5cfd50d0011f0360d7201b6fa9e3
Kevin Chen <ningchen227@gmail.com>
2019-04-23 20:55:37 -0500

    Updated CSV to SQL converter

--- csv_import.py
+++ csv_import.py
@@ -1,13 +1,18 @@
 import csv
 import MySQLdb as sql_db
 
-gym_db = sql_db.connect(host='localhost', user='root', passwd='', db='gym', autocommit=True)
+gym_db = sql_db.connect(host='localhost', user='root', passwd='kumar$123', db='gym', autocommit=True)
 cursor = gym_db.cursor()
 
 data = csv.reader(open('Database.csv'))
 for row in data:
     row_type = row[0]
     row.pop(0)
+    print(row_type)
+    if row_type == 'MEMBER':
+        print("Imported a MEMBER tuple with values:", row)
+        cursor.execute('INSERT INTO MEMBER ' \
+        'VALUES(%s, %s, %s, %s, %s, %s, %s, "%s");', row)
 
     if row_type == 'ROOM':
         row = list(map(int, row)) # Converts all values to integers
@@ -15,16 +20,18 @@ for row in data:
         cursor.execute('INSERT INTO ROOM ' \
         'VALUES(%s, %s);', row)
 
+    '''
     if row_type == 'ATTENDS': # SQL Note: A MEMBER must be initialized with MemberID in order for this to work
         row = list(map(int, row)) # Converts all values to integers
         print("Imported an ATTENDS tuple with values:", row)
         cursor.execute('INSERT INTO ATTENDS ' \
         'VALUES(%s, %s);', row)
-    
+
     if row_type == 'TRAINER_Certification':
         print("Imported a TRAINER_Certification tuple with values:", row)
         cursor.execute('INSERT INTO TRAINER_Certification ' \
         'VALUES(%s, %s);', row)
+    '''
 
 cursor.close()
 print('Done')
