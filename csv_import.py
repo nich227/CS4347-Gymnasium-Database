b19c72117793f94b8e042bdd76d5a48fea8def21
Kevin Chen <ningchen227@gmail.com>
2019-04-24 14:42:55 -0500

    Completed CSV to SQL import py file

--- csv_import.py
+++ csv_import.py
@@ -8,11 +8,37 @@ data = csv.reader(open('Database.csv'))
 for row in data:
     row_type = row[0]
     row.pop(0)
-    print(row_type)
+
     if row_type == 'MEMBER':
         print("Imported a MEMBER tuple with values:", row)
         cursor.execute('INSERT INTO MEMBER ' \
-        'VALUES(%s, %s, %s, %s, %s, %s, %s, "%s");', row)
+        'VALUES(%s, %s, %s, %s, %s, %s, %s, %s);', row)
+    
+    if row_type == 'ATTENDS':
+        row = list(map(int, row)) # Converts all values to integers
+        print("Imported an ATTENDS tuple with values:", row)
+        cursor.execute('INSERT INTO ATTENDS ' \
+        'VALUES(%s, %s);', row)
+    
+    if row_type == 'MEMBER_Waiver':
+        print("Imported a MEMBER_Waiver tuple with values:", row)
+        cursor.execute('INSERT INTO MEMBER_Waiver ' \
+        'VALUES(%s, %s);', row)
+    
+    if row_type == 'TRAINER':
+        print("Imported a TRAINER tuple with values:", row)
+        cursor.execute('INSERT INTO TRAINER ' \
+        'VALUES(%s, %s, %s, %s, %s);', row)
+    
+    if row_type == 'TRAINER_Certification':
+        print("Imported a TRAINER_Certification tuple with values:", row)
+        cursor.execute('INSERT INTO TRAINER_Certification ' \
+        'VALUES(%s, %s);', row)
+    
+    if row_type == 'ROOM_CType':
+        print("Imported a ROOM_CType tuple with values:", row)
+        cursor.execute('INSERT INTO ROOM_CType ' \
+        'VALUES(%s, %s);', row)
 
     if row_type == 'ROOM':
         row = list(map(int, row)) # Converts all values to integers
@@ -20,18 +46,13 @@ for row in data:
         cursor.execute('INSERT INTO ROOM ' \
         'VALUES(%s, %s);', row)
 
-    '''
-    if row_type == 'ATTENDS': # SQL Note: A MEMBER must be initialized with MemberID in order for this to work
-        row = list(map(int, row)) # Converts all values to integers
-        print("Imported an ATTENDS tuple with values:", row)
-        cursor.execute('INSERT INTO ATTENDS ' \
-        'VALUES(%s, %s);', row)
 
-    if row_type == 'TRAINER_Certification':
-        print("Imported a TRAINER_Certification tuple with values:", row)
-        cursor.execute('INSERT INTO TRAINER_Certification ' \
-        'VALUES(%s, %s);', row)
-    '''
+    if row_type == 'CLASS':
+        print("Imported a CLASS tuple with values:", row)
+        cursor.execute('INSERT INTO CLASS ' \
+        'VALUES(%s, %s, %s, %s, %s, %s, %s);', row)
+
+
 
 cursor.close()
 print('Done')
